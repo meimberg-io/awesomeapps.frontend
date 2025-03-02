5477d3904a58fec67123ac177538353a6b8e8bf0
OliverMeimberg <oli@meimberg.io>
2025-03-02 22:01:03 +0100

    nextjs eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../../styles/index.css"; // oder "../styles/main.css", je nach Struktur
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 
 
+export const BASE_URL = (typeof process !== 'undefined' && process.env.STRAPI_PUBLIC_BASE_URL) ? process.env.STRAPI_PUBLIC_BASE_URL : 'http://localhost:1337';
 
-export     const BASE_URL = (typeof process !== 'undefined' && process.env.STRAPI_PUBLIC_BASE_URL) ? process.env.STRAPI_PUBLIC_BASE_URL : 'http://localhost:1337';
+console.log("STRAPI_PUBLIC_BASE_URL: ", process.env.STRAPI_PUBLIC_BASE_URL)
 
-
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({Component, pageProps}: AppProps) {
     return <Component {...pageProps} />;
 }
