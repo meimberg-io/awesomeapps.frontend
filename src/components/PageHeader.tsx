8a8fdf051b66f60d838e8659198e34ed2dee1596
OliverMeimberg <oli@meimberg.io>
2025-03-07 19:08:14 +0100

    styling refactor

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React from 'react'
+import {STRAPI_BASEURL} from "../pages/_app";
+
+export enum PageHeaderStyle {
+    SERVICE = 'service',
+    PAGE = 'page',
+}
+
+export interface PageHeaderProps {
+    title: string;
+    subtitle: string | null;
+    icon?: string | null;
+    style: PageHeaderStyle;
+
+}
+
+const PageHeader: React.FC<PageHeaderProps> = ({title, subtitle, icon, style}) => {
+
+    let style_color_bg: string = "";
+    let style_color_text: string = "";
+
+
+    switch (style) {
+        case PageHeaderStyle.SERVICE:
+            style_color_bg = "bg-blue-50";
+            style_color_text = "text-blue-950";
+            break;
+        case PageHeaderStyle.PAGE:
+            style_color_bg = "bg-red-50";
+            style_color_text = "text-red-950";
+            break;
+        default:
+            style_color_bg = "bg-grey-50";
+            style_color_text = "text-grey-950";
+            break;
+    }
+
+    return (
+        <header className={`py-8  ${style_color_bg} `}>
+            <div className="mx-auto flex max-w-7xl flex-row items-stretch gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8 justify-between">
+                <div >
+                    <h1 className={`text-xl mb-0 mt-0 font-semibold ${style_color_text}`}>{title}</h1>
+                    <p className={`text-sm mt-2 ${style_color_text}`}>{subtitle}</p>
+                </div>
+                {
+                    icon && (
+                        <figure className="w-20 h-20 rounded-full bg-white flex items-center justify-center ">
+                            <img src={STRAPI_BASEURL + icon} alt={title} className="max-h-10 max-w-10 "/>
+                        </figure>
+                    )
+                }
+            </div>
+        </header>
+
+
+    )
+}
+export default PageHeader;
--- src/components/strapicomponents/richtextblocks/RichTextBlocks.tsx
+++ src/components/strapicomponents/richtextblocks/RichTextBlocks.tsx
@@ -24,19 +24,19 @@ const RichTextBlocks: React.FC<RichTextProps> = ({content}) => {
                 heading: ({children, level}) => {
                     switch (level) {
                         case 1:
-                            return <h1 className="text-4xl font-bold mb-8 mt-6">{children}</h1>
+                            return <h1>{children}</h1>
                         case 2:
-                            return <h2 className="text-3xl font-semibold mb-8 mt-5">{children}</h2>
+                            return <h2>{children}</h2>
                         case 3:
-                            return <h3 className="text-xl font-medium mb-4 mt-8">{children}</h3>
+                            return <h3>{children}</h3>
                         case 4:
-                            return <h4 className="text-lg font-medium mb-2 mt-2">{children}</h4>
+                            return <h4>{children}</h4>
                         case 5:
-                            return <h5 className="text-lg font-medium mb-2 mt-2">{children}</h5>
+                            return <h5>{children}</h5>
                         case 6:
-                            return <h6 className="text-lg font-medium mb-2 mt-2">{children}</h6>
+                            return <h6>{children}</h6>
                         default:
-                            return <h1 className="text-4xl font-bold">{children}</h1>
+                            return <h1>{children}</h1>
                     }
                 },
                 // Für Links kannst du die Komponente deines Routers oder Frameworks verwenden
