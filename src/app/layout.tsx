1de42488b10ff6a9705b12f4f28ef2c787025741
OliverMeimberg <oliver.meimberg@gmail.com>
2025-04-06 20:15:02 +0200

    matomo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 
 import '@/styles/index.css'
 import {ReactNode} from 'react'
+import {MatomoTracker} from "@/components/util/MatomoTracker";
 
 export default function RootLayout({children}: { children: ReactNode }) {
     return (
         <html lang="de" data-theme="dark">
         <body className="h-full">
         {children}
+        <MatomoTracker />
+
         </body>
         </html>
     )
