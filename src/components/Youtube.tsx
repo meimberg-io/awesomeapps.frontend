a44f94039b28c112d6a2d1806da1d20b5e06199d
OliverMeimberg <oli@meimberg.io>
2025-04-11 00:05:53 +0200

    youtube 16:9

--- src/components/Youtube.tsx
+++ src/components/Youtube.tsx
@@ -4,9 +4,16 @@ const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 export default function Youtube({ video, title }: { video: string; title?: string }) {
     return (
-        <div className="w-full overflow-hidden rounded-lg">
-            {title && <h2>Video: {title}</h2>}
-            <ReactPlayer url={'https://www.youtube.com/watch?v=' + video} width="100%" />
+
+
+        <div className="relative w-full pb-[56%]">
+            <div className="absolute top-0 left-0 w-full h-full">
+                <ReactPlayer url={'https://www.youtube.com/watch?v=' + video} width="100%" height="100%"/>
+            </div>
         </div>
+        // <div className="w-full overflow-hidden rounded-lg">
+        //     {title && <h2>Video: {title}</h2>}
+        //     <ReactPlayer url={'https://www.youtube.com/watch?v=' + video} width="100%" height="100%"/>
+        // </div>
     );
 }
