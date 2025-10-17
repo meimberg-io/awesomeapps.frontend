5d80969c882fef16e66f33d2c7ea43fd7021c08b
OliverMeimberg <oliver.meimberg@gmail.com>
2025-10-17 18:29:59 +0200

    one more ssr fix

---
+++ src/app/[locale]/favorites/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function FavoritesLayout({ children }: { children: ReactNode }) {
+  return children;
+}
+
