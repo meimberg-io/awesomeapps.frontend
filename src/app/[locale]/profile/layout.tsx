8c8b1f14288092d5d452d4be1dc64504e3cbc095
OliverMeimberg <oli@meimberg.io>
2025-10-17 18:51:42 +0200

    one more ssr fix

--- src/app/[locale]/profile/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-export default function ProfileLayout({ children }: { children: ReactNode }) {
-  return children;
-}
-
